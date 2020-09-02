e7480fbd1051361f51444b0aa765fb5644bca39c
LuigiSampaio <cursosinformatica9102@gmail.com>
2020-09-01 22:03:01 -0300

    feat(Starting the Project)

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,8 +1,84 @@
-const getUSer = async () => ({
-    name: 'nome do usuário',
-    idade: 21
-});
+const knex = require('knex');
+const tableName = 'usuarios';
+
+let queryBuilder;
+
+const connectDatabase = async () => {
+    queryBuilder = queryBuilder || knex({
+        client: 'pg',
+        connection: 'postgres://URL',
+        searchPath: ['knex', 'public'],
+    });
+
+    return queryBuilder;
+}
+
+// Estrutura para visualizar users no DB 
+const getUSer = async () => {
+    const queryBuilder = await connectDatabase();
+
+    return queryBuilder.select('*').from(tableName);
+};
+
+// Estrutura para criar users no DB
+const createUSer = async ({ 
+    name, 
+    lastname 
+}) => {
+    const queryBuilder = await connectDatabase();
+
+    return queryBuilder(tableName).insert({
+        name,
+        lastname
+    });
+};
+
+
+// Estrutura para atualizar users no DB
+const updateUser = async ({ id }, { name, lastname }) => {
+
+    try {
+        const queryBuilder = await connectDatabase();
+
+        await queryBuilder(tableName)
+            .where({ id })
+            .update({
+                name, 
+                lastname
+            });
+
+            return {
+                message:'Update feito com Sucesso'
+            }
+    } catch (error) {
+        throw new Error('Deu ruim');
+    }
+};
+
+
+// Estrutura para deletar users no DB
+const deleteUser = async ({ id }) => {
+
+    try {
+        const queryBuilder = await connectDatabase();
+
+        await queryBuilder(tableName)
+            .where({ id })
+            .del ();
+
+        return {
+            message: 'Usuário removido com Sucesso'
+        };
+
+    } catch (error) {
+        throw new Error('Deu ruim')
+    };
+    
+};
 
 module.exports = {
-    getUSer
+    getUSer,
+    createUSer,
+    updateUser,
+    deleteUser
 }
--- src/routes.js
+++ src/routes.js
@@ -11,4 +11,22 @@ routes.get('/getUser', (req, res) => controller.getUSer()
     .catch(error => console.error(error))
 );
 
+routes.post('/createUser', (req, res) =>
+    controller.createUSer(req.body)
+        .then(result => res.send(result))
+        .catch(error => console.error(error))
+);
+
+routes.put('/updateUser/:id', (req, res) =>
+    controller.updateUser(req.params, req.body)
+        .then(result => res.send(result))
+        .catch(error => console.error(error))
+);
+
+routes.delete('/deleteUser/:id', (req, res) =>
+    controller.deleteUser(req.params)
+        .then(result => res.send(result))
+        .catch(error => console.error(error))
+);
+
 module.exports = routes;
